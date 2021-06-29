92ca66605ce2ae5ab951bffa0a08c8e6cb36c4df
rktechie255 <ratnesh.dsi@gmail.com>
2021-06-29 22:46:26 +0530

    stage-3

---
+++ src/main/java/org/rktechie/spring5di/Spring5DependencyInjectionApplication.java
@@ -0,0 +1,13 @@
+package org.rktechie.spring5di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5DependencyInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5DependencyInjectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/rktechie/spring5di/Spring5DependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rktechie.spring5di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5DependencyInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
